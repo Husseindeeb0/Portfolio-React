daea9d0490eab1b7b857287c0fa27afdb71203d8
Hussein Deeb <deebh776@gmail.com>
2025-10-11 18:14:43 +0300

    Add project to portfolio

--- Portfolio/src/pages/Portfolio/index.jsx
+++ Portfolio/src/pages/Portfolio/index.jsx
@@ -24,6 +24,7 @@ import image_4 from "/assets/images/test_generator.png";
 import image_6 from "/assets/images/bitwiseclub.png";
 import image_5 from "/assets/images/litechat.png";
 import image_3 from "/assets/images/shirtmod3d.png";
+import image_7 from "/assets/images/aidea-home.png";
 import "./index.scss";
 
 const Portfolio = () => {
@@ -34,7 +35,7 @@ const Portfolio = () => {
       image: image_6,
       title: "BitwiseClub",
       githubLink: "https://github.com/husseindeeb0/Bitwise",
-      liveLink: "https://bitwiseclub.com",
+      liveLink: "https://aidea-lb.com",
       tools: [
         <FontAwesomeIcon icon={faHtml5} key="html" color="#e34c26" />,
         <FontAwesomeIcon icon={faCss3Alt} key="css" color="#1572b6" />,
@@ -48,6 +49,23 @@ const Portfolio = () => {
     },
     {
       id: 2,
+      image: image_7,
+      title: "Aidea",
+      githubLink: null,
+      liveLink: "https://bitwiseclub.com",
+      tools: [
+        <FontAwesomeIcon icon={faHtml5} key="html" color="#e34c26" />,
+        <FontAwesomeIcon icon={faCss3Alt} key="css" color="#1572b6" />,
+        <SiTailwindcss key="tailwind" color="#38b2ac" />,
+        <SiTypescript key="typescript" color="#3178C6" />,
+        <FontAwesomeIcon icon={faReact} key="react" color="#61dafb" />,
+        <SiMongodb key="mongoDb" color="#4DB33D" />,
+        <FontAwesomeIcon icon={faNodeJs} key="node" color="#8cc84b" />,
+        <SiExpress key="express" color="#ffff" />,
+      ],
+    },
+    {
+      id: 3,
       image: image_5,
       title: "LiteChat",
       githubLink: "https://github.com/Husseindeeb0/LiteChat",
@@ -64,7 +82,7 @@ const Portfolio = () => {
       ],
     },
     {
-      id: 3,
+      id: 4,
       image: image_4,
       title: "Test Generator",
       githubLink: "https://github.com/husseindeeb0/Test-Generator",
@@ -81,7 +99,7 @@ const Portfolio = () => {
       ],
     },
     {
-      id: 4,
+      id: 5,
       image: image_3,
       title: "ShirtMod3D",
       githubLink: "https://github.com/husseindeeb0/ShirtMod3D",
@@ -97,7 +115,7 @@ const Portfolio = () => {
       ],
     },
     {
-      id: 5,
+      id: 6,
       image: image_2,
       title: "Food Recipe",
       githubLink: "https://github.com/husseindeeb0/Food-Recipe-React",
@@ -111,7 +129,7 @@ const Portfolio = () => {
       ],
     },
     {
-      id: 6,
+      id: 7,
       image: image_1,
       title: "Facebook Clone",
       githubLink: "https://github.com/husseindeeb0/Facebook-Clone",
@@ -161,15 +179,22 @@ const Portfolio = () => {
                 ))}
               </div>
               <div className="project-actions">
+                {project.githubLink && (
+                  <a
+                    href={project.githubLink}
+                    target="_blank"
+                    className="icon"
+                    title="Code source"
+                  >
+                    <FontAwesomeIcon icon={faGithub} color="black" />
+                  </a>
+                )}
                 <a
-                  href={project.githubLink}
+                  href={project.liveLink}
                   target="_blank"
-                  className="icon"
-                  title="Code source"
+                  className="button"
+                  style={{ marginTop: project.githubLink ? "" : "5px" }}
                 >
-                  <FontAwesomeIcon icon={faGithub} color="black" />
-                </a>
-                <a href={project.liveLink} target="_blank" className="button">
                   Visit
                 </a>
               </div>
