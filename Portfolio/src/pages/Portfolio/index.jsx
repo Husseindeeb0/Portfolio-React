39b0e606028e75a2cd8e2a75e706f4b52895a22e
Hussein <deebh776@gmail.com>
2025-11-15 17:06:39 +0200

    Fix live links in portfolio

--- Portfolio/src/pages/Portfolio/index.jsx
+++ Portfolio/src/pages/Portfolio/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import {
   SiExpress,
   SiMongodb,
@@ -6,9 +6,9 @@ import {
   SiSocketdotio,
   SiTypescript,
   SiDaisyui,
-} from "react-icons/si";
-import AnimatedLetters from "../../components/AnimatedLetters";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+} from 'react-icons/si'
+import AnimatedLetters from '../../components/AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faGithub,
   faHtml5,
@@ -16,26 +16,26 @@ import {
   faJsSquare,
   faReact,
   faNodeJs,
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons'
 
-import image_1 from "/assets/images/facebook.png";
-import image_2 from "/assets/images/recipe.jpg";
-import image_4 from "/assets/images/test_generator.png";
-import image_6 from "/assets/images/bitwiseclub.png";
-import image_5 from "/assets/images/litechat.png";
-import image_3 from "/assets/images/shirtmod3d.png";
-import image_7 from "/assets/images/aidea-home.png";
-import "./index.scss";
+import image_1 from '/assets/images/facebook.png'
+import image_2 from '/assets/images/recipe.jpg'
+import image_4 from '/assets/images/test_generator.png'
+import image_6 from '/assets/images/bitwiseclub.png'
+import image_5 from '/assets/images/litechat.png'
+import image_3 from '/assets/images/shirtmod3d.png'
+import image_7 from '/assets/images/aidea-home.png'
+import './index.scss'
 
 const Portfolio = () => {
-  const [letterClass, setLetterClass] = useState("text-animate");
+  const [letterClass, setLetterClass] = useState('text-animate')
   const projects = [
     {
       id: 1,
       image: image_6,
-      title: "BitwiseClub",
-      githubLink: "https://github.com/husseindeeb0/Bitwise",
-      liveLink: "https://aidea-lb.com",
+      title: 'BitwiseClub',
+      githubLink: 'https://github.com/husseindeeb0/Bitwise',
+      liveLink: 'https://bitwiseclub.com',
       tools: [
         <FontAwesomeIcon icon={faHtml5} key="html" color="#e34c26" />,
         <FontAwesomeIcon icon={faCss3Alt} key="css" color="#1572b6" />,
@@ -50,9 +50,9 @@ const Portfolio = () => {
     {
       id: 2,
       image: image_7,
-      title: "Aidea",
+      title: 'Aidea',
       githubLink: null,
-      liveLink: "https://bitwiseclub.com",
+      liveLink: 'https://aidea-lb.com',
       tools: [
         <FontAwesomeIcon icon={faHtml5} key="html" color="#e34c26" />,
         <FontAwesomeIcon icon={faCss3Alt} key="css" color="#1572b6" />,
@@ -67,9 +67,9 @@ const Portfolio = () => {
     {
       id: 3,
       image: image_5,
-      title: "LiteChat",
-      githubLink: "https://github.com/Husseindeeb0/LiteChat",
-      liveLink: "https://litechat-70j2.onrender.com",
+      title: 'LiteChat',
+      githubLink: 'https://github.com/Husseindeeb0/LiteChat',
+      liveLink: 'https://litechat-70j2.onrender.com',
       tools: [
         <SiTailwindcss key="tailwind" color="#38b2ac" />,
         <SiDaisyui key="daisyui" color="#1ad1a5" />,
@@ -84,9 +84,9 @@ const Portfolio = () => {
     {
       id: 4,
       image: image_4,
-      title: "Test Generator",
-      githubLink: "https://github.com/husseindeeb0/Test-Generator",
-      liveLink: "https://test-generator-frontend.onrender.com",
+      title: 'Test Generator',
+      githubLink: 'https://github.com/husseindeeb0/Test-Generator',
+      liveLink: 'https://test-generator-frontend.onrender.com',
       tools: [
         <FontAwesomeIcon icon={faHtml5} key="html" color="#e34c26" />,
         <FontAwesomeIcon icon={faCss3Alt} key="css" color="#1572b6" />,
@@ -101,15 +101,15 @@ const Portfolio = () => {
     {
       id: 5,
       image: image_3,
-      title: "ShirtMod3D",
-      githubLink: "https://github.com/husseindeeb0/ShirtMod3D",
-      liveLink: "https://shirtmod3d-frontend.onrender.com",
+      title: 'ShirtMod3D',
+      githubLink: 'https://github.com/husseindeeb0/ShirtMod3D',
+      liveLink: 'https://shirtmod3d-frontend.onrender.com',
       tools: [
         <FontAwesomeIcon icon={faHtml5} key="html" color="#e34c26" />,
         <SiTailwindcss key="tailwind" color="#38b2ac" />,
         <SiTypescript key="typescript" color="#3178C6" />,
         <FontAwesomeIcon icon={faReact} key="react" color="#61dafb" />,
-        <img src="threejs.png" alt="threejs Logo" style={{ width: "30px" }} />,
+        <img src="threejs.png" alt="threejs Logo" style={{ width: '30px' }} />,
         <FontAwesomeIcon icon={faNodeJs} key="node" color="#8cc84b" />,
         <SiExpress key="express" color="#ffff" />,
       ],
@@ -117,9 +117,9 @@ const Portfolio = () => {
     {
       id: 6,
       image: image_2,
-      title: "Food Recipe",
-      githubLink: "https://github.com/husseindeeb0/Food-Recipe-React",
-      liveLink: "https://husseindeeb0.github.io/Food-Recipe-React/",
+      title: 'Food Recipe',
+      githubLink: 'https://github.com/husseindeeb0/Food-Recipe-React',
+      liveLink: 'https://husseindeeb0.github.io/Food-Recipe-React/',
       tools: [
         <FontAwesomeIcon icon={faHtml5} key="html" color="#e34c26" />,
         <FontAwesomeIcon icon={faCss3Alt} key="css" color="#1572b6" />,
@@ -131,9 +131,9 @@ const Portfolio = () => {
     {
       id: 7,
       image: image_1,
-      title: "Facebook Clone",
-      githubLink: "https://github.com/husseindeeb0/Facebook-Clone",
-      liveLink: "https://social-clone0.netlify.app/",
+      title: 'Facebook Clone',
+      githubLink: 'https://github.com/husseindeeb0/Facebook-Clone',
+      liveLink: 'https://social-clone0.netlify.app/',
       tools: [
         <FontAwesomeIcon icon={faHtml5} key="html" color="#e34c26" />,
         <FontAwesomeIcon icon={faCss3Alt} key="css" color="#1572b6" />,
@@ -142,17 +142,17 @@ const Portfolio = () => {
         <FontAwesomeIcon icon={faReact} key="react" color="#61dafb" />,
       ],
     },
-  ];
+  ]
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLetterClass("text-animate-hover");
-    }, 3000);
+      setLetterClass('text-animate-hover')
+    }, 3000)
 
     return () => {
-      clearTimeout(timer);
-    };
-  });
+      clearTimeout(timer)
+    }
+  })
 
   return (
     <>
@@ -160,7 +160,7 @@ const Portfolio = () => {
         <h1 className="page-title">
           <AnimatedLetters
             letterClass={letterClass}
-            strArray={"Portfolio".split("")}
+            strArray={'Portfolio'.split('')}
             idx={15}
           />
         </h1>
@@ -193,7 +193,7 @@ const Portfolio = () => {
                   href={project.liveLink}
                   target="_blank"
                   className="button"
-                  style={{ marginTop: project.githubLink ? "" : "5px" }}
+                  style={{ marginTop: project.githubLink ? '' : '5px' }}
                 >
                   Visit
                 </a>
@@ -203,7 +203,7 @@ const Portfolio = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Portfolio;
+export default Portfolio
